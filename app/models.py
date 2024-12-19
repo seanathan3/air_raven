63212222eb4771d8268f230c8bb76556908f1a9f
seanathan3 <sean.abbas33@gmail.com>
2024-12-19 16:02:14 -0500

    start models

--- app/models.py
+++ app/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Flight(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    flight_number = models.CharField(max_length=20)
+    carrier = models.CharField(max_length=50)
+
+
