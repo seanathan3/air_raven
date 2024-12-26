971d61369d5b214b52e8dbe2d7cdd3f20fdaee40
seanathan3 <sean.abbas33@gmail.com>
2024-12-26 09:50:22 -0600

    api troubleshooting

--- app/flights.py
+++ app/flights.py
@@ -1,9 +1,13 @@
+import json
 import requests
 from opensky_api import OpenSkyApi
 
-url = "https://www.example.com"
+url = "https://opensky-network.org/api/flights/departure?airport=KJFK&begin=1734696060&end=1734699660"
 
 file = requests.get(url)
 
-print(file.text)
-
+if file is not None:
+    arr = json.loads(file.text)
+    for i, record in enumerate(len(arr)):
+        print(record.)
+    
